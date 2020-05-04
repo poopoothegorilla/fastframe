36e6050a13f0186fab8336af863ba88187ac4031
poopoothegorilla <jm.wlkr1@gmail.com>
2020-05-03 20:50:59 -0400

    dataframe: add example

--- dataframe/example_dataframe_test.go
+++ dataframe/example_dataframe_test.go
@@ -11,7 +11,7 @@ import (
 
 func generateUsersDF(pool memory.Allocator) dataframe.DataFrame {
 	schema := arrow.NewSchema([]arrow.Field{
-		arrow.Field{Name: "user_id", Type: arrow.PrimitiveTypes.Int64},
+		arrow.Field{Name: "id", Type: arrow.PrimitiveTypes.Int64},
 		arrow.Field{Name: "age", Type: arrow.PrimitiveTypes.Int32},
 	}, nil)
 
@@ -21,17 +21,47 @@ func generateUsersDF(pool memory.Allocator) dataframe.DataFrame {
 	defer bi32.Release()
 
 	bi64.AppendValues([]int64{1000, 21407, 898989, 8888, 101292}, nil)
+	ids := bi64.NewArray()
+	defer ids.Release()
+
 	bi32.AppendValues([]int32{20, 16, 35, 66, 50}, nil)
+	ages := bi32.NewArray()
+	defer ages.Release()
 
-	arri64 := bi64.NewArray()
-	defer arri64.Release()
-	arri32 := bi32.NewArray()
-	defer arri32.Release()
-
-	cols := []array.Interface{arri64, arri32}
+	cols := []array.Interface{ids, ages}
 	users := []array.Record{array.NewRecord(schema, cols, -1)}
 
 	return dataframe.NewFromRecords(pool, users)
+}
+
+func generateMovieRatingsDF(pool memory.Allocator) dataframe.DataFrame {
+	schema := arrow.NewSchema([]arrow.Field{
+		arrow.Field{Name: "user_id", Type: arrow.PrimitiveTypes.Int64},
+		arrow.Field{Name: "movie_id", Type: arrow.PrimitiveTypes.Int64},
+		arrow.Field{Name: "rating", Type: arrow.PrimitiveTypes.Int32},
+	}, nil)
+
+	bi64 := array.NewInt64Builder(pool)
+	defer bi64.Release()
+	bi32 := array.NewInt32Builder(pool)
+	defer bi32.Release()
+
+	bi64.AppendValues([]int64{1000, 21407, 898989, 8888, 101292}, nil)
+	userIDs := bi64.NewArray()
+	defer userIDs.Release()
+
+	bi64.AppendValues([]int64{1000, 21407, 898989, 8888, 101292}, nil)
+	movieIDs := bi64.NewArray()
+	defer movieIDs.Release()
+
+	bi32.AppendValues([]int32{20, 16, 35, 66, 50}, nil)
+	ratings := bi32.NewArray()
+	defer ratings.Release()
+
+	cols := []array.Interface{userIDs, movieIDs, ratings}
+	movieRatings := []array.Record{array.NewRecord(schema, cols, -1)}
+
+	return dataframe.NewFromRecords(pool, movieRatings)
 }
 
 func Example() {
@@ -53,10 +83,10 @@ func Example() {
 	}
 
 	// Output:
-	// rec[0]["user_id"]: [1000 21407]
+	// rec[0]["id"]: [1000 21407]
 	// rec[0]["age"]: [20 16]
-	// rec[1]["user_id"]: [898989 8888]
+	// rec[1]["id"]: [898989 8888]
 	// rec[1]["age"]: [35 66]
-	// rec[2]["user_id"]: [101292]
+	// rec[2]["id"]: [101292]
 	// rec[2]["age"]: [50]
 }
